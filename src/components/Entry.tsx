2fdd84c5acc2dda28c7046f9acdd6436b646de4d
FooOperator <foxoperator1@gmail.com>
2022-09-12 23:02:33 +0100

    Working on ghost visual functionality for dnd

---
+++ src/components/Entry.tsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { EntryType } from "../store";
+
+export const Entry = ({
+	entry,
+	ghost = false,
+}: {
+	entry: EntryType;
+	ghost?: boolean;
+}) => {
+	return (
+		<img
+			className={`entry object-cover w-36 h-36 ${
+				ghost ? "opacity-75" : ""
+			}`}
+			onDragStart={(e) => {
+				console.log("dragStart");
+				e.dataTransfer.setData("id", entry[0]);
+				e.dataTransfer.setData("src", entry[1]);
+			}}
+			onDragEnd={(e) => {
+				console.log("dragEnd");
+				e.dataTransfer.clearData("id");
+				e.dataTransfer.clearData("src");
+			}}
+			src={entry[1]}
+			id={entry[0]}
+			alt=""
+		/>
+	);
+};
