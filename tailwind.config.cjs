803381e6582a0929295a3e8b6c6e917d2bcd96ec
FooOperator <foxoperator1@gmail.com>
2022-09-15 19:16:20 +0100

    new utilities
    template columns should be refactored.
    
    some button utilities are conflicting.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ["./src/**/*.tsx"],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns: {
+				tier: "repeat(5, minmax(120px, 1fr))",
+				unranked : 'repeat(3, minmax(65px, 1fr))'
+			},
+		},
 	},
 	plugins: [],
 };
